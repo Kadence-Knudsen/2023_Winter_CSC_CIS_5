6a47750b313611e2eb5a6ede9ddd5b3cb61bcb7c
Kadence Knudsen <kknudsen2@student.rccd.edu>
2023-01-08 20:23:12 -0800

    Signed-off-by: Kadence Knudsen <kknudsen2@student.rccd.edu>

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: kadence
+ * Created on January 8, 2023, 5:51 PM
+ * Purpose: Hello World
+ *    First Program all programmers write 
+ *    When testing their IDR for the first 
+ *    time. 
+ *    C:Programming Language by
+ *    Dennis Ritchie and Brian Kernighan
+ */
+
+//System Libraries 
+#include <Iostream> //Input Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants not Variables
+//Science, Math, Conversions, Dimensions 
+
+//Function Prototypes 
+
+//Execution begins here at main 
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables 
+    
+    //Initialize Variables 
+    
+    //Map/Process the Inputs-> Outputs 
+    
+    //Display Inputs/Outputs 
+    cout<<"Hello World"<<endl;
+    //Clean up memory and files 
+    
+    //Exit the Program 
+    return 0;
+}
+
